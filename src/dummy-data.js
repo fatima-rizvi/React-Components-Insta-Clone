a929564654e59dbab7b30b51d856af4856fd6bfb
Fatima Rizvi <fatimarzv23@gmail.com>
2020-09-08 21:32:42 -0500

    Added data to dummy data

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -54,6 +54,81 @@ const dummyData = [
             },
         ],
     },
+    {
+        id: 3,
+        username: "bethsCorner",
+        thumbnailUrl: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/02/pumpkin-3f3d894.jpg?quality=90&resize=960,872',
+        imageUrl: 'https://images.unsplash.com/photo-1598974152323-68f6d00f20a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=662&q=80',
+        likes: 276,
+        timestamp: "July 16th 2017, 10:23:30 pm",
+        comments: [
+            {
+                id: 42,
+                username: "bethsCorner",
+                text: "Ready to relax!",
+            },
+            {
+                id: 43,
+                username: "biancasaurus",
+                text: "Oooh, what scent is the candle?",
+            },
+            {
+                id: 44,
+                username: "fashion_tips",
+                text: "Love the decor!",
+            },
+        ],
+    },
+    {
+        id: 4,
+        username: "nasa",
+        thumbnailUrl: 'https://media.glassdoor.com/sqll/7304/nasa-squarelogo.png',
+        imageUrl: 'https://images.unsplash.com/photo-1462331940025-496dfbfc7564?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=827&q=80',
+        likes: 12104,
+        timestamp: "July 15th 2017, 04:17:24 pm",
+        comments: [
+            {
+                id: 52,
+                username: "nasa",
+                text: "Observing our universe in different kinds of light #space",
+            },
+            {
+                id: 53,
+                username: "coolGuyCooper",
+                text: "How do I get this as my desktop",
+            },
+            {
+                id: 54,
+                username: "ellenrng",
+                text: "Looks beautiful!!!",
+            },
+        ],
+    },
+    {
+        id: 5,
+        username: "SeaWorld",
+        thumbnailUrl: 'https://www.nfwf.org/sites/default/files/styles/logo_large_557px_/public/2020-01/seaworld.jpg?itok=WLwN3qvC',
+        imageUrl: 'https://images.unsplash.com/photo-1573350926865-cf0d512fd504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+        likes: 467,
+        timestamp: "July 17th 2017, 06:47:53 pm",
+        comments: [
+            {
+                id: 62,
+                username: "SeaWorld",
+                text: "Come say hi to the sting rays!",
+            },
+            {
+                id: 63,
+                username: "nicholas_bulpit",
+                text: "*waves back to the stingray*",
+            },
+            {
+                id: 64,
+                username: "charlottejones",
+                text: "Went to the aquarium today, definitely stopped by the sting rays!",
+            },
+        ],
+    }
 ];
 
 export default dummyData;
